88a078790c12f857217f74fc7b2d8185b8d2f3c7
Danjoe4 <djbroderick4@gmail.com>
2022-05-02 13:40:04 -0400

    final

--- public/js/blog.js
+++ public/js/blog.js
@@ -84,3 +84,17 @@ function getPosts() {
         });
 }
 getPosts();
+
+$(document).ready(()=>{
+    $.getJSON('/get_current_user').done(data=>{
+        if(data.message==='success'){
+            console.log(data)
+            const user=data.data;
+            $('.login').remove();
+            $('#showname').text(user.fullname)
+            $('#showname2').text(user.fullname)
+        }else{
+            $('.logout').remove();
+        }
+    })
+})

0493be3693a09e7c239b51467a8b7331b4c4033b
Danjoe4 <djbroderick4@gmail.com>
2022-05-04 12:24:03 -0400

    server fix

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Configure Mongoose, and listen on the port, this is better for deployment
-mongoose.connect('mongodb://localhost:27017/extractiveDB', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
     app.listen(port, ()=>{
         console.log("Database connection is Ready "
